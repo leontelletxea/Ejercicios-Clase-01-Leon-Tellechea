422bcc023ac7c53abaa56e7e342cb3fe75f36487
leontelletxea <leontelletxea@gmail.com>
2021-08-27 10:46:48 -0300

    Modificaciones Heroku

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'TITULO';
+  edadUno: number;
+  edadDos: number;
+  promedio: number;
+  suma: number;
+
+  constructor() {
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
+  Calcular() {
+    this.promedio = ((this.edadUno + this.edadDos) / 2);
+    this.suma = this.edadUno + this.edadDos;
+  }
+
+  Limpiar() {
+    this.title = 'TITULO';
+    this.edadUno = 0;
+    this.edadDos = 0;
+    this.promedio = 0;
+    this.suma = 0;
+  }
+}
+
+export class Usuario {
+  nombre:string;
+  clave:string;
+
+  constructor(){
+    this.nombre = "";
+    this.clave = "";
+  }
+}
