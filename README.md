5e35ad3d5e8494283abd57d0e8ec06b0f3a97ba7
leontelletxea <leontelletxea@gmail.com>
2021-08-30 22:33:18 -0300

    Modificaciones Heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LeonTellecheaLabo4
+# clase-01-leon-tellechea
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/leon-tellechea-labo4'),
+      dir: require('path').join(__dirname, './coverage/clase-01-leon-tellechea'),
       subdir: '.',
       reporters: [
         { type: 'html' },
