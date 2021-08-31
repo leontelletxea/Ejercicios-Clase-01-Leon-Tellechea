5f42703222b7d7cd8ebc3f74be61c8a965525589
leontelletxea <leontelletxea@gmail.com>
2021-08-30 22:21:44 -0300

    Modificaciones Heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/leon-tellechea-labo4'));
+app.use(express.static(__dirname + '/dist/Ejercicios-Clase-01-Leon-Tellechea'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/dist/leon-tellechea-labo4/index.html'));
+res.sendFile(path.join(__dirname + '/dist/Ejercicios-Clase-01-Leon-Tellechea/index.html'));
 });
 console.log('Build successful!!');
